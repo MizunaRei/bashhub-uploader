5eefbaa3af89027a7af3f44da634f4851dd1a9b2
MilkyMAISHIRANUI <dr_liu0806@msn.com>
2022-10-23 22:37:24 +0900

    Add files via upload

---
+++ import-to-bashhub.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env zsh
+
+filename=$1
+export unixtimenow
+export linescounter
+
+
+## count the total number of lines in file. start
+linescounter=0
+while read line; do
+linescounter=$((linescounter+1))
+done < $filename
+# linescounter end
+
+
+## read and upload line by line. start.
+n=0
+while read line; do
+# reading each line
+# echo $line
+unixtimenow=$(bashhub util parsedate $(date +"%Y-%m-%dT%H:%M:%S%z"))
+bashhub save "${line}" ~/ "$$" "${unixtimenow}"  0
+n=$((n+1))
+ echo -e " \e[1A\e[K ${n} of ${linescounter} entries imported "
+## printf " \e[1A \e[K ${n} of ${linescounter} entries imported "
+## pause 1s to save some server resource
+# sleep 1
+done < $filename
+## read and upload . end
+
+printf " \n ${n} entries imported into bashhub.com \n "
